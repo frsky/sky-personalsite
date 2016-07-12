fa6b7c1936320f4a591b143dbe3905713f3f3fa9
frsky <frank@iasqu.com>
2016-07-11 20:17:37 -0400

    Editorial changes

--- index.php
+++ index.php
@@ -6,12 +6,12 @@ $mooq = file_put_contents($moof, $logline, FILE_APPEND);
 ?>
 <!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Frank Sikernitsky</title>
 <meta name="description" content="A fun and unique look at the major products and projects of noted engineer and entrepreneur Frank Sikernitsky.">
-<meta name="apple-mobile-web-app-capable" content="yes"  /><meta name=viewport content="width=device-width,shrink-to-fit=no">
+<meta name="apple-mobile-web-app-capable"  content="yes"  /><meta  name=viewport content="width=device-width,shrink-to-fit=no">
 <script>
 var cb = function() {
 var l = document.createElement('link'); l.rel = 'stylesheet';
 l.href = 'jquery-ui-1.8.20.custom.min.css';
-var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
+var h = document.getElementsByTagName('head')[0];  h.parentNode.insertBefore(l, h);
 };
 var raf = requestAnimationFrame || mozRequestAnimationFrame ||
 webkitRequestAnimationFrame || msRequestAnimationFrame;
