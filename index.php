d24e02f5a3dc12e610342e9e2203b57c8bc38eaa
frsky <frank@iasqu.com>
2016-07-27 20:56:35 -0400

    Optimize

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
-$t9 = $_SERVER['DOCUMENT_ROOT'];
-$moof="$t9/fiplog.log";
+$t9 = $_SERVER['DOCUMENT_ROOT']; $moof="$t9/fiplog.log";
 $logline = date("Y-m-d H:i:s") . "\t" . $_SERVER['REMOTE_ADDR'] . "\t" . "FRANK" . "\t" .  $_SERVER['HTTP_USER_AGENT'] . "\r\n";
 $mooq = file_put_contents($moof, $logline, FILE_APPEND);
 ?>
 <!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Frank Sikernitsky</title>
 <meta name = "description" content="A fun and unique look at the major products and projects of noted engineer and entrepreneur Frank Sikernitsky.">
-<meta name = "apple-mobile-web-app-capable"  content="yes"  /><meta  name=viewport content="width=device-width,shrink-to-fit=no">
-<script>   
+<meta name = "apple-mobile-web-app-capable" content="yes" /><meta  name=viewport content="width=device-width,shrink-to-fit=no">
+<script>
 var cb = function() {
 var l = document.createElement('link'); l.rel = 'stylesheet';
 l.href = 'jquery-ui-1.8.20.custom.min.css';
